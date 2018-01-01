72d68e9603925f80fa95fb373008fe7018b439b7
Avartil <33838459+Avartil@users.noreply.github.com>
2018-01-01 23:48:02 +0100

    Add files via upload

---
+++ frontend/js/view.js
@@ -0,0 +1,102 @@
+/*global qs, qsa, $on, $parent, $delegate */
+
+(function (window) {
+	'use strict';
+
+	/**
+	     * View that abstracts away the browser's DOM completely.
+	     * It has two simple entry points:
+	     *
+	     *   - bind(eventName, handler)
+	     *     Takes a todo application event and registers the handler
+	     *   - render(command, parameterObject)
+	     *     Renders the given command with the options
+	     */
+	function View(template) {
+		this.template = template;
+
+		this.ENTER_KEY = 13;
+		this.ESCAPE_KEY = 27;
+
+		this.$todoList = qs('.todo-list');
+		this.$main = qs('.main');
+		this.$newTodo = qs('.new-todo');
+		this.$addNew = qs('.addNew');
+	}
+
+	View.prototype._removeItem = function (id) {
+		var elem = qs('[data-id="' + id + '"]');
+
+		if (elem) {
+			this.$todoList.removeChild(elem);
+		}
+	};
+
+
+	View.prototype._elementComplete = function (id, completed) {
+		var listItem = qs('[data-id="' + id + '"]');
+
+		if (!listItem) {
+			return;
+		}
+
+		listItem.className = completed ? 'completed' : '';
+
+		// In case it was toggled from an event and not by clicking the checkbox
+		qs('input', listItem).checked = completed;
+
+	};
+
+
+	View.prototype.render = function (viewCmd, parameter) {
+		var self = this;
+		var viewCommands = {
+			showEntries: function () {
+				self.$todoList.innerHTML = self.template.show(parameter);
+			},
+			removeItem: function () {
+				self._removeItem(parameter);
+			},
+			clearNewTodo: function () {
+				self.$newTodo.value = '';
+			},
+			elementComplete: function () {
+				self._elementComplete(parameter.id, parameter.completed);
+			}
+		};
+
+		viewCommands[viewCmd]();
+	};
+
+	View.prototype._itemId = function (element) {
+		var li = $parent(element, 'li');
+		return parseInt(li.dataset.id, 10);
+	};
+
+	View.prototype.bind = function (event, handler) {
+		var self = this;
+		if (event === 'newTodo') {
+			$on(self.$newTodo, 'change', function () {
+				handler(self.$newTodo.value);
+			});
+
+		} else if (event === 'itemRemove') {
+			$delegate(self.$todoList, '.destroy', 'click', function () {
+				handler({id: self._itemId(this)});
+			});
+
+		} else if (event === 'itemToggle') {
+			$delegate(self.$todoList, '.toggle', 'click', function () {
+				handler({
+					id: self._itemId(this),
+					completed: this.checked
+				});
+			});
+
+		} 
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.View = View;
+}(window));
